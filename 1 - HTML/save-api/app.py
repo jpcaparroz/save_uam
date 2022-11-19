5fb5d6432e4a9d7faf8a2495f7c46fc998cb5ccf
GustavoManfredo <work.manfredo@gmail.com>
2022-11-19 18:17:50 -0300

    feat: adicionando ao backend

---
+++ 1 - HTML/save-api/app.py
@@ -0,0 +1,157 @@
+import json
+import sqlite3
+
+from flask import Flask, request, render_template
+from flask import g
+from flask import jsonify, make_response
+
+dataServer = './save-bd.db'
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+def carregaBanco():
+    banco = getattr(g, '_database', None)
+
+    if banco is None:
+        g._database = sqlite3.connect(dataServer)
+        banco = g._database
+
+        return banco
+
+def criarTabela():
+    banco = carregaBanco()
+    scriptTabela = open ('schemas\schema.sql').read()
+    banco.executescript(scriptTabela)
+
+@app.route('/criartabelas')
+def index():
+    banco = carregaBanco
+    criarTabela()
+    return "<h1>Tabelas Criadas :)</h1>"
+
+@app.route('/addUser', methods = ['POST',])
+def addUsuario():
+    banco = carregaBanco()
+    cur = banco.cursor()
+
+    email = request.form["email"]
+    nome = request.form["nome"]
+    
+    cur.execute("""
+    INSERT INTO Usuario (email, nome)
+    VALUES (?,?)""",
+    (email, nome))
+    
+    banco.commit()
+    banco.close()
+
+    return ("Nome: %s, Email: %s" % (email, nome))
+
+@app.route('/registrar', methods = ['GET',])
+def registrar():
+    
+
+    return render_template('registrar.html')
+
+@app.route('/')
+def login():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/filmes')
+def filmes():
+    return render_template('filmes.html')
+
+@app.route('/pesquisar')
+def pesquisar():
+    return render_template('pesquisar.html')
+
+@app.route('/recomendacoes')
+def recomendacoes():
+    return render_template('recomendacoes.html')
+########################################################################################################
+##############                            API                                             ##############
+########################################################################################################
+
+#API que retorna usuarios do banco 
+@app.route('/api/usuario', methods = ['GET',])
+def apiUsuario():
+    banco = carregaBanco()
+    cur = banco.cursor()
+
+    result = cur.execute("""
+    SELECT * FROM usuario 
+    """,
+    ).fetchall()
+    
+    columns = [desc[0] for desc in cur.description]
+    results = []
+
+    for row in result:
+        row = dict(zip(columns, row))
+        results.append(row)
+    
+    banco.commit()
+    banco.close()
+
+    return jsonify (results)
+
+#API que retorna filmes que o usuario já assistiu
+@app.route('/api/filmeusuario', methods = ['GET',])
+def apiFilmeUsuario():
+    banco = carregaBanco()
+    cur = banco.cursor()
+
+    result = cur.execute("""
+    SELECT * FROM filmeusuario  
+    """,
+    ).fetchall()
+
+    columns = [desc[0] for desc in cur.description]
+    results = []
+
+    for row in result:
+        row = dict(zip(columns, row))
+        results.append(row)
+    
+    banco.commit()
+    banco.close()
+
+    return jsonify (results)
+
+@app.route('/api/top250', methods = ['GET',])
+def apiTop250():
+
+    with open('250.json', 'r') as data:
+        top250 = json.load(data)
+
+    colums = ['title', 'year', 'image']
+    results = []
+
+    i = 0
+    limit = 100
+
+    for film in top250['items']:
+
+        title = film['title']
+        year = film['year']
+        image = film['image']
+
+        film = [title, year, image]
+
+        film = dict(zip(colums,film))
+        results.append(film)
+        i+= 1
+
+        if i == limit:
+            break
+
+    return jsonify(results)
---
+++ 1 - HTML/save-api/static/pesquisar/js/main.js
@@ -0,0 +1,110 @@
+const recommendPage = {
+    saveApi: `http://127.0.0.1:5000/api/usuario`,
+    searchBar: document.querySelector('.search-bar'),
+    apiKey: '8cdb9794',
+    imdbApi(apiKey, userSearch) {
+        return `https://www.omdbapi.com/?apikey=${apiKey}&s=${userSearch}`
+    }
+}
+const {searchBar, saveApi, imdbApi, apiKey} = recommendPage
+
+authLogin()
+
+searchBar.addEventListener('keydown', (e) => {
+    
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        const userInput = searchBar.value
+        removeMovieList()
+        connectImdbApi(imdbApi(apiKey, userInput))
+        searchAnimations()
+    }
+})
+
+function connectSaveApi(url, userEmail) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const index = data.map(object => object.email).indexOf(userEmail)
+        const item = data[index]
+
+        if(index === -1) {
+            console.table("Usuario não encontrado... Redirecionando ao login => :)")
+            return window.location.href = '/'
+        } else {
+            const userEmailApi = item.email
+            const userNameApi = item.nome
+    
+            authData(userEmailApi, userNameApi)
+        }
+
+    })
+    .catch(err => console.error(err))
+}
+
+function authData(userEmailApi, userNameApi) {
+    const getLocalStorageName = localStorage.getItem('name')
+    const getLocalStorageEmail = localStorage.getItem('email')
+
+    if(!getLocalStorageEmail || !getLocalStorageName) {
+        console.log("Dados incompletos...")
+        return window.location.href = '/'
+    } else if(getLocalStorageEmail != userEmailApi || getLocalStorageName != userNameApi) {
+        removeLocalStorageData()
+        console.log("Dados não conferem com ao da API...");
+        return window.location.href = '/'
+    } else {
+        console.log('Usuario autenticado');
+    }
+}
+
+function removeLocalStorageData () {
+    localStorage.removeItem('name')
+    localStorage.removeItem('email')
+}
+
+function authLogin() {
+    const email = localStorage.getItem('email')
+    connectSaveApi(saveApi, email)
+}
+
+function connectImdbApi(url) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        let list = data.Search
+
+        list.map((item) => {
+            let title = item.Title
+            let year = item.Year
+            let poster = item.Poster
+            let email = localStorage.getItem('email')
+
+            if(poster === 'N/A') {
+                poster = 'https://t4.ftcdn.net/jpg/02/14/06/71/360_F_214067110_eB6LNUMWR8nSXSTEG1SSpJGfkS7c9zMd.jpg'
+            }
+
+            createMoviesList(title, year, poster, email)
+            
+        })
+    })
+    .catch(err => console.error(err))
+}
+
+function createMoviesList(title, year, poster, email) {
+    let movie = `<div class="movie-wrapper"><form action="" method="post"><div class="userContent-wrapper"><div class="hidden-content"><input type="hidden" name="emailUsuario" value="${email}"></div><div class="image-wrapper"><img src="${poster}"> <input type="hidden" name="posterFilme" value="${poster}"></div></div><div class="content-wrapper"><div class="input-wrapper"><h1>${title}</h1><input type="hidden" name="nomeFilme" value="${title}"></div><div class="input-wrapper"><h2>${year}</h2><input type="hidden" name="anoFilme" value="${year}"></div><div class="input-wrapper"><p>Nota:</p><input class="rating" max="5" step="1" type="range" value="0" name="notaFilme" oninput='this.style.setProperty("--value",this.value)'></div><button type="submit"><svg width="45" height="53" viewBox="0 0 59 53" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29.3333 52.0002C30.75 52.0002 57.6666 37.8341 57.6666 18.0013C57.6666 8.08495 50.5833 1.12481 42.0833 1.00182C37.8333 0.940329 33.5833 2.41684 29.3333 8.08351C25.0833 2.41684 20.7598 1.00182 16.5833 1.00182C8.08333 1.00182 1 8.08496 1 18.0013C1 37.8341 27.9167 52.0002 29.3333 52.0002Z" fill="#E63946" stroke="#E63946" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button></div></form></div>`
+    document.querySelector('.movies-container').innerHTML += movie
+}
+
+function searchAnimations() {
+    document.querySelector('.search-bar').classList.remove('middle')
+    document.querySelector('.movies-container').classList.remove('hide')
+}
+
+function removeMovieList() {
+    let div = document.querySelector('.movies-container');
+
+    div.innerHTML = ""
+}
