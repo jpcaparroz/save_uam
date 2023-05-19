1011b2dcb21c1c3a46d1ccb9f8a66e5e95eb86b0
GustavoManfredo <gustavmanfred17@gmail.com>
2023-05-19 00:03:07 -0300

    Mudanca

--- Save2.0/save-serverJava/src/main/java/com/api/server/rmi/modules/Usuario.java
+++ Save2.0/save-serverJava/src/main/java/com/api/server/rmi/modules/Usuario.java
@@ -2,12 +2,14 @@ package com.api.server.rmi.modules;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Usuario {
+public class Usuario implements Serializable {
     private String email;
     private String nome;
 }
