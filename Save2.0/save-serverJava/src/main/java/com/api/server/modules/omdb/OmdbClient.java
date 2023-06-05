7fa27b40c72941cb0889682336b435126c7be1d7
GustavoManfredo <gustavmanfred17@gmail.com>
2023-06-05 19:49:42 -0300

    Adding /movies to the url path

--- Save2.0/save-serverJava/src/main/java/com/api/server/modules/omdb/OmdbClient.java
+++ Save2.0/save-serverJava/src/main/java/com/api/server/modules/omdb/OmdbClient.java
@@ -11,11 +11,11 @@ import java.util.List;
 @FeignClient(name = "omdbClient", url = "http://127.0.0.1:5000")
 public interface OmdbClient {
 
-    @GetMapping("/search")
+    @GetMapping("/movies/search")
     ResponseEntity<List<MoviesDto>> searchMovies(@RequestParam("s") String s,
                                                  @RequestParam(value = "page", required = false, defaultValue = "1") String page);
 
-    @GetMapping("/topChart")
+    @GetMapping("/movies/topChart")
     ResponseEntity<List<MoviesDto>> getTopChart();
 
 }
